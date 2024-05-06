4b756c40ad2337717d5bee1775839f09ffe19e81
joyfunc <joyfunc@gmail.com>
2024-05-06 20:40:45 +0800

    Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -104,10 +104,7 @@ export function Hero() {
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
-              <Button
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                variant="outline"
-              >
+              <Button href="https://youtu.be/bHgz7tAeWns" variant="outline">
                 <PlayIcon className="h-6 w-6 flex-none" />
                 <span className="ml-2.5">Watch the video</span>
               </Button>
